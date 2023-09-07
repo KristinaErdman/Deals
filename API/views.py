5053642f504d266f6b64a00f04d3dac5e445737f
Kristina Tabunchik <tabunchik@doanddot.com>
2023-09-07 22:46:13 +0700

    add search with case - insensitive to save_deals_from_file

--- API/views.py
+++ API/views.py
@@ -51,10 +51,25 @@ class APIDeals(APIView):
         try:
             reader = csv.DictReader(codecs.iterdecode(input_file, 'utf-8'))
             for line in reader:
-                new_customer, created = Customer.objects.get_or_create(username=line['customer'].lower().strip())
-                new_item, created = Gem.objects.get_or_create(name=line['item'].lower().strip())
-                new_deal = Deal.objects.create(customer=new_customer, item=new_item, total=line['total'],
-                                               quantity=line['quantity'], date=line['date'])
+                line['customer'] = line['customer'].strip()
+                new_customer, created = Customer.objects.get_or_create(
+                    username__iexact=line['customer'],
+                    defaults={'username': line['customer']}
+                )
+
+                line['item'] = line['item'].strip()
+                new_item, created = Gem.objects.get_or_create(
+                    name__iexact=line['item'],
+                    defaults={'name': line['item']}
+                )
+
+                new_deal = Deal.objects.create(
+                    customer=new_customer,
+                    item=new_item,
+                    total=line['total'],
+                    quantity=line['quantity'],
+                    date=line['date']
+                )
                 new_deal.save()
         except KeyError:
             raise KeyError('The file content does not match the desired format')
